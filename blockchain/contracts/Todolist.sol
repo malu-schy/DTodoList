3e46c7abd18371fa57f18564355f1fa6d89f8a2b
Pavithran Gridharan <pavethiran@gmail.com>
2021-05-14 02:07:26 +0200

    add toggle functionality

--- blockchain/contracts/Todolist.sol
+++ blockchain/contracts/Todolist.sol
@@ -2,78 +2,83 @@
 pragma solidity ^0.8.3;
 import "hardhat/console.sol";
 
-contract Todolist{
-  TodoItem[] public todos; 
-  string todo1;
+contract Todolist {
+    TodoItem[] public todos;
+    string todo1;
 
-  struct TodoItem{
-    uint id; 
-    string text;
-    bool closed; 
-  }
+    struct TodoItem {
+        uint256 id;
+        string text;
+        bool closed;
+    }
 
-  uint public todoCount; 
+    uint256 public todoCount;
 
-  constructor(string memory _todo) {
-    todoCount = 0; 
-    console.log("Deploying a TodoList with greeting:", _todo);
-    todo1 = _todo;
-    todos.push(TodoItem(0,"hello Hardhat",true));
-    // todos.push(TodoItem(1,"hello Lalalal"));
-    // todos.push(TodoItem(2,"hello Solidity"));
-    // getTodoListLength();
-    // getTodoItem(0);
-  }
+    constructor(string memory _todo) {
+        todoCount = 0;
+        console.log("Deploying a TodoList with greeting:", _todo);
+        todo1 = _todo;
+        todos.push(TodoItem(0, "hello Hardhat", true));
+        // todos.push(TodoItem(1,"hello Lalalal"));
+        // todos.push(TodoItem(2,"hello Solidity"));
+        // getTodoListLength();
+        // getTodoItem(0);
+    }
 
-  function getTodoListLength() public view returns (uint){
-    console.log("Array hat eine Laenge von: ", todos.length);
-    return todos.length; 
-  }
+    function getTodoListLength() public view returns (uint256) {
+        console.log("Array hat eine Laenge von: ", todos.length);
+        return todos.length;
+    }
 
-  function getTodoItem(uint _id) public view returns (string memory){
-    return todos[_id].text;
-  }
+    function getTodoItem(uint256 _id) public view returns (string memory) {
+        return todos[_id].text;
+    }
 
-  function isTodoClosed(uint _id) public view returns (bool) {
-    // if(todos[_id].closed == false){
-    //   return 0;
-    // } else {
-    //   return 1;
+    function isTodoClosed(uint256 _id) public view returns (bool) {
+        // if(todos[_id].closed == false){
+        //   return 0;
+        // } else {
+        //   return 1;
+        // }
+        return todos[_id].closed;
+    }
+
+    // function getAllElementsOfArray() public view returns(TodoItem[] memory){
+    //   return todos;
     // }
-    return todos[_id].closed;
-  }
-  // function getAllElementsOfArray() public view returns(TodoItem[] memory){
-  //   return todos;
-  // }
 
-  // function todo(uint _id) public view returns (string memory) {
-  //  string memory textItem;
-  //   for (uint i = 0; i <= todos.length; i++) {
-  //       if(_id == todos[i].id){
-  //         textItem = todos[i].text;
-  //         // textItem = "Aloha";
-  //           console.log(textItem);
-  //       }
-  //   } return textItem; 
-  // }  
+    // function todo(uint _id) public view returns (string memory) {
+    //  string memory textItem;
+    //   for (uint i = 0; i <= todos.length; i++) {
+    //       if(_id == todos[i].id){
+    //         textItem = todos[i].text;
+    //         // textItem = "Aloha";
+    //           console.log(textItem);
+    //       }
+    //   } return textItem;
+    // }
 
-  // function todo() public view returns (string memory) {
-  //   for (uint i = 0; i <= todos.length; i++) {
-  //       console.log("toodooo1 ", todo1);
-  //   }
-  //   return todo1; 
-  // }
+    // function todo() public view returns (string memory) {
+    //   for (uint i = 0; i <= todos.length; i++) {
+    //       console.log("toodooo1 ", todo1);
+    //   }
+    //   return todo1;
+    // }
 
-  function setTodo(string memory _todo) public {
-    console.log("extend todolist with: ", _todo);
-    TodoItem memory newTodoItem; 
-    newTodoItem.id = todos.length -1; 
-    newTodoItem.text = _todo; 
-    newTodoItem.closed = false; 
-    todos.push(newTodoItem);
-  }
+    function createTodo(string memory _todo) public {
+        console.log("extend todolist with: ", _todo);
+        TodoItem memory newTodoItem;
+        newTodoItem.id = todos.length - 1;
+        newTodoItem.text = _todo;
+        newTodoItem.closed = false;
+        todos.push(newTodoItem);
+    }
 
-  function deleteTodo(string memory _todo) public {
-    console.log("delete TODO");
-  }
+    function toggleTodo(uint256 _id) public {
+        todos[_id].closed = !todos[_id].closed;
+    }
+
+    function deleteTodo(string memory _todo) public {
+        console.log("delete TODO");
+    }
 }
--- client/src/Pages/Todolist.js
+++ client/src/Pages/Todolist.js
@@ -1,81 +1,107 @@
-import React from "react";
-import Todoliste from '../artifacts/contracts/Todolist.sol/Todolist.json'
-import { useState } from 'react';
-import { ethers } from 'ethers';
+import * as React from "react";
+import { ethers } from "ethers";
+import Todoliste from "../artifacts/contracts/Todolist.sol/Todolist.json";
 
-const todoListAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const todoListAddress = process.env.REACT_APP_ADDRESS;
 
- function TodolistComponent () {
-    const todoListe =  []; 
-    const [outputText, setOutputText] = useState([]);
-    const [todo, setNewTodo] = useState('');
+function TodolistComponent() {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  const [refresh, setRefresh] = React.useState(0);
 
-    async function requestAccount() {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-      }
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
   
-    async function fetchTodos() {
-        if (typeof window.ethereum !== 'undefined') {
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
-          const contract = new ethers.Contract(todoListAddress, Todoliste.abi, provider)
-          try {
-            const todoListLength = await contract.getTodoListLength();
-            for(var i = 0; i < todoListLength; i++){
-              const data = await contract.getTodoItem(i)
-              // const data = await contract.getTodoItem(i)
-              todoListe.push(data);
-              console.log(data);
-            }
-            // const data = await contract.getTodoItem(3)
-            // console.log("TodoListe hat: " + todoListLength + " Einträge");
-            console.log(todoListe);
-            setOutputText(todoListe);
-          } catch (err) {
-            console.log("Oh no Error: ", err)
-          }
-        }    
-      }
-
-      async function setTodo() {
-        if (!todo) return
-        if (typeof window.ethereum !== 'undefined') {
-          await requestAccount()
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner()
-          const contract = new ethers.Contract(todoListAddress, Todoliste.abi, signer)
-          const transaction = await contract.setTodo(todo)
-          await transaction.wait()
-          fetchTodos()
+  async function fetchTodos() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        todoListAddress,
+        Todoliste.abi,
+        provider
+      );
+      try {
+        const todoListLength = await contract.getTodoListLength();
+        const list = [];
+        for (var i = 0; i < todoListLength; i++) {
+          const text = await contract.getTodoItem(i);
+          const closed = await contract.isTodoClosed(i);
+          list.push({ text, closed });
         }
+        setTodos(list);
+      } catch (err) {
+        console.log("Oh no Error: ", err);
       }
+    }
+  }
 
-      function deleteTodo(){
-        console.log("Ich bin gelöscht");
-      }
+  async function createTodo() {
+    if (!todo) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        todoListAddress,
+        Todoliste.abi,
+        signer
+      );
+      const transaction = await contract.createTodo(todo);
+      await transaction.wait();
+      setRefresh(refresh + 1);
+    }
+  }
 
-    return (
-        <div className="App">
-        <h1> Decentralized TodoList 💫 </h1> 
-            <div className="TodolistInput">
-                <input onChange={e => setNewTodo(e.target.value)} placeholder="Hello" />
-                <button onClick={setTodo}>Add Todo</button>
-                <button onClick={fetchTodos}>Refresh TodoList</button>
-            </div>   
-            <div className="TodolistOutput">
-              <label>Your Todos:</label>
-              {outputText.map((item, _key) => {
-              return <div className="todoItem" key={_key}>
-                    {
-                     <input type="checkbox" onChange={deleteTodo}></input>
-                     /* the following will remove qoutes from string: (Thank you Stackoverlow :D <3)
-                     replace(/^"(.+(?="$))"$/, '$1') */ }
-                      {JSON.stringify(item).replace(/^"(.+(?="$))"$/, '$1')}
-                     </div>
-                    })
-                 }
-              </div>
-       </div>
-    );
+  async function toggleTodo(id) {
+    if (!todo) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        todoListAddress,
+        Todoliste.abi,
+        signer
+      );
+      const transaction = await contract.toggleTodo(id);
+      await transaction.wait();
+      setRefresh(refresh + 1);
+    }
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(() => fetchTodos(), [refresh]);
+
+  return (
+    <div className="App">
+      <h1> Decentralized TodoList 💫 </h1>
+      <div className="TodolistInput">
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="New Todo Name"
+          value={todo}
+        />
+        <button onClick={createTodo}>Add Todo</button>
+        <button onClick={fetchTodos}>Refresh TodoList</button>
+      </div>
+      <div className="TodolistOutput">
+        <label>Your Todos:</label>
+        {todos.map((item, _key) => {
+          return (
+            <div className="todoItem" key={_key}>
+              {
+                <input type="checkbox" onChange={() => toggleTodo(_key)} checked={item.closed}></input>
+                /* the following will remove qoutes from string: (Thank you Stackoverlow :D <3)
+                     replace(/^"(.+(?="$))"$/, '$1') */
+              }
+              {JSON.stringify(item.text).replace(/^"(.+(?="$))"$/, "$1")}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default TodolistComponent;
